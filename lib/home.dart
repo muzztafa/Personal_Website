c81ee863c3367e3557dc445a6f11bc8e8a53b41d
Ahmed Mustafa <ahmedmustafa5832@gmail.com>
2022-08-29 16:36:52 -0400

    home without avatar

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'colors.dart';
-import 'package:side_navigation/side_navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -67,64 +69,68 @@ class _HomeState extends State<Home> {
       //   ],
       // ),
       appBar: AppBar(
-          title: Text("AM",
-              style: GoogleFonts.exo(
-                textStyle: Theme.of(context).textTheme.headline4,
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
-                color: primaryColor,
-              )),
+          toolbarHeight: 100,
+          title: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text("AM",
+                style: GoogleFonts.exo(
+                  textStyle: Theme.of(context).textTheme.headline4,
+                  fontSize: 50,
+                  fontWeight: FontWeight.w700,
+                  color: primaryColor,
+                )),
+          ),
           backgroundColor: Colors.transparent,
           elevation: 0,
           actions: [
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(20.0),
               child: GestureDetector(
                 onTap: () {},
                 child: Text("Home",
                     style: GoogleFonts.exo(
                       textStyle: Theme.of(context).textTheme.headline4,
                       fontSize: 16,
-                      fontWeight: FontWeight.w700,
+                      // fontWeight: FontWeight.w700,
                       color: primaryColor,
                     )),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(20.0),
               child: GestureDetector(
                 onTap: () {},
                 child: Text("About",
                     style: GoogleFonts.exo(
                       textStyle: Theme.of(context).textTheme.headline4,
                       fontSize: 16,
-                      fontWeight: FontWeight.w700,
+                      // fontWeight: FontWeight.w700,
                       color: primaryColor,
                     )),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(20.0),
               child: GestureDetector(
                 onTap: () {},
                 child: Text("Skills",
                     style: GoogleFonts.exo(
                       textStyle: Theme.of(context).textTheme.headline4,
                       fontSize: 16,
-                      fontWeight: FontWeight.w700,
+                      // fontWeight: FontWeight.w700,
                       color: primaryColor,
                     )),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(20.0),
               child: GestureDetector(
                 onTap: () {},
                 child: Text("Projects",
                     style: GoogleFonts.exo(
                       textStyle: Theme.of(context).textTheme.headline4,
                       fontSize: 16,
-                      fontWeight: FontWeight.w700,
+                      // fontWeight: FontWeight.w700,
                       color: primaryColor,
                     )),
               ),
@@ -156,6 +162,12 @@ class Main extends StatelessWidget {
               ),
             ),
             Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 250, 0),
+                child: Align(
+                    alignment: Alignment.topRight,
+                    child: Image(
+                        image: const AssetImage("images/avatar-removed.png")))),
+            Padding(
               padding: const EdgeInsets.fromLTRB(200, 200, 0, 0),
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -164,29 +176,106 @@ class Main extends StatelessWidget {
                     Text("Hi,",
                         style: GoogleFonts.exo(
                           textStyle: Theme.of(context).textTheme.headline4,
-                          fontSize: 42,
+                          fontSize: 35,
                           fontWeight: FontWeight.w700,
                           color: primaryColor,
                         )),
                     Text("I'm Mustafa.",
                         style: GoogleFonts.exo(
                           textStyle: Theme.of(context).textTheme.headline4,
-                          fontSize: 42,
+                          fontSize: 35,
                           fontWeight: FontWeight.w700,
                           color: primaryColor,
                         )),
-                    Text("A Software Engineer.",
+                    Text("A Problem Solver.",
                         style: GoogleFonts.exo(
                           textStyle: Theme.of(context).textTheme.headline4,
-                          fontSize: 42,
+                          fontSize: 35,
                           fontWeight: FontWeight.w700,
                           color: primaryColor,
-                        ))
+                        )),
+                    Container(
+                      height: 25,
+                      width: 25,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            launchUrlString("https://github.com/muzztafa");
+                          },
+                          child: Text("Github",
+                              style: GoogleFonts.exo(
+                                textStyle: TextStyle(
+                                    decoration: TextDecoration.underline),
+                                fontSize: 20,
+                                color: Colors.black54,
+                              )),
+                        ),
+                        Text("     |     ",
+                            style: GoogleFonts.exo(
+                              fontSize: 20,
+                              color: Colors.black54,
+                            )),
+                        InkWell(
+                          onTap: () {
+                            launchUrlString(
+                                "https://linkedin.com/in/ahmedmustafa--");
+                          },
+                          child: Text("LinkedIn",
+                              style: GoogleFonts.exo(
+                                textStyle: TextStyle(
+                                    decoration: TextDecoration.underline),
+                                fontSize: 20,
+                                color: Colors.black54,
+                              )),
+                        ),
+                      ],
+                    )
                   ]),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class HoverButton extends StatefulWidget {
+  const HoverButton({Key? key}) : super(key: key);
+
+  @override
+  State<HoverButton> createState() => _HoverButtonState();
+}
+
+class _HoverButtonState extends State<HoverButton> {
+  @override
+  Widget build(BuildContext context) {
+    bool isHover = false;
+    return AnimatedContainer(
+        height: 70,
+        duration: Duration(milliseconds: 200),
+        padding: EdgeInsets.only(
+            top: (isHover) ? 25 : 30.0, bottom: !(isHover) ? 25 : 30),
+        child: Container(
+          height: 30,
+          color: Colors.blue,
+          child: InkWell(
+            onTap: () {},
+            child: Text("Hover Button"),
+            onHover: (val) {
+              print("Val--->{}$val");
+              setState(() {
+                isHover = val;
+              });
+            },
+          ),
+          /*val--->true when user brings in mouse
+           val---> false when brings out his mouse*/
+        ));
   }
 }
