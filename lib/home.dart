637c13ba0068e9aed568127f38ead352a1cf027f
Ahmed Mustafa <ahmedmustafa5832@gmail.com>
2022-08-29 17:39:16 -0400

    about page done

--- lib/home.dart
+++ lib/home.dart
@@ -146,100 +146,189 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double mediaHeight = MediaQuery.of(context).size.height;
+    double mediaWidth = MediaQuery.of(context).size.width;
+
     return SingleChildScrollView(
-      child: Expanded(
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
-              child: Center(
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.25,
-                  height: MediaQuery.of(context).size.width * 0.25,
-                  decoration:
-                      BoxDecoration(shape: BoxShape.circle, color: bgWhite),
+      child: Column(
+        children: [
+          Home(mediaHeight, mediaWidth, context),
+          About(mediaHeight, mediaWidth, context),
+          Skills(mediaHeight, mediaWidth, context),
+        ],
+      ),
+    );
+  }
+
+  Container Skills(
+      double mediaHeight, double mediaWidth, BuildContext context) {
+    return Container(
+      height: mediaHeight,
+      width: mediaWidth,
+      child: Text("skills"),
+    );
+  }
+
+  Container About(double mediaHeight, double mediaWidth, BuildContext context) {
+    return Container(
+      height: mediaHeight,
+      width: mediaWidth,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(100, 0, 250, 0),
+            child: Text("About",
+                style: GoogleFonts.exo(
+                    textStyle: Theme.of(context).textTheme.headline4,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w700,
+                    color: primaryColor,
+                    decoration: TextDecoration.overline)),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(200, 100, 0, 0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("A little about myself:",
+                            style: GoogleFonts.exo(
+                              textStyle: Theme.of(context).textTheme.headline4,
+                              fontSize: 35,
+                              fontWeight: FontWeight.w500,
+                              color: primaryColor,
+                            )),
+                        Text(
+                            "Hi! I'm Ahmed Mustafa, a passionate software engineer who loves to play with data. I enjoy building high quality software applications that are bound to scale. I am currently pursuing my Masters in Applied Computing and Artificial Intelligence from the University of Windsor (Graduating in June 2023) and am actively looking for 2023 Winter internships/coops.",
+                            style: GoogleFonts.exo(
+                              textStyle: Theme.of(context).textTheme.headline4,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                              color: primaryColor,
+                            )),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        Text("Always up for a new challenge! ",
+                            style: GoogleFonts.exo(
+                              textStyle: Theme.of(context).textTheme.headline4,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                              color: primaryColor,
+                            )),
+                      ]),
                 ),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.topCenter,
+                        child:
+                            Image(image: const AssetImage("images/iba.jpg"))))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container Home(double mediaHeight, double mediaWidth, BuildContext context) {
+    return Container(
+      height: mediaHeight,
+      width: mediaWidth,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.width * 0.5,
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: bgWhite),
               ),
             ),
-            Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 250, 0),
-                child: Align(
-                    alignment: Alignment.topRight,
-                    child: Image(
-                        image: const AssetImage("images/avatar-removed.png")))),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(200, 200, 0, 0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Hi,",
-                        style: GoogleFonts.exo(
-                          textStyle: Theme.of(context).textTheme.headline4,
-                          fontSize: 35,
-                          fontWeight: FontWeight.w700,
-                          color: primaryColor,
-                        )),
-                    Text("I'm Mustafa.",
-                        style: GoogleFonts.exo(
-                          textStyle: Theme.of(context).textTheme.headline4,
-                          fontSize: 35,
-                          fontWeight: FontWeight.w700,
-                          color: primaryColor,
-                        )),
-                    Text("A Problem Solver.",
-                        style: GoogleFonts.exo(
-                          textStyle: Theme.of(context).textTheme.headline4,
-                          fontSize: 35,
-                          fontWeight: FontWeight.w700,
-                          color: primaryColor,
-                        )),
-                    Container(
-                      height: 25,
-                      width: 25,
-                      color: Colors.black,
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            launchUrlString("https://github.com/muzztafa");
-                          },
-                          child: Text("Github",
-                              style: GoogleFonts.exo(
-                                textStyle: TextStyle(
-                                    decoration: TextDecoration.underline),
-                                fontSize: 20,
-                                color: Colors.black54,
-                              )),
-                        ),
-                        Text("     |     ",
+          ),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 250, 0),
+              child: Align(
+                  alignment: Alignment.topRight,
+                  child:
+                      Image(image: const AssetImage("images/sidepose.png")))),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(200, 200, 0, 0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Hi,",
+                      style: GoogleFonts.exo(
+                        textStyle: Theme.of(context).textTheme.headline4,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w700,
+                        color: primaryColor,
+                      )),
+                  Text("I'm Mustafa.",
+                      style: GoogleFonts.exo(
+                        textStyle: Theme.of(context).textTheme.headline4,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w700,
+                        color: primaryColor,
+                      )),
+                  Text("A Problem Solver.",
+                      style: GoogleFonts.exo(
+                        textStyle: Theme.of(context).textTheme.headline4,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w700,
+                        color: primaryColor,
+                      )),
+                  Container(
+                    height: 25,
+                    width: 25,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          launchUrlString("https://github.com/muzztafa");
+                        },
+                        child: Text("Github",
                             style: GoogleFonts.exo(
+                              textStyle: TextStyle(
+                                  decoration: TextDecoration.underline),
                               fontSize: 20,
                               color: Colors.black54,
                             )),
-                        InkWell(
-                          onTap: () {
-                            launchUrlString(
-                                "https://linkedin.com/in/ahmedmustafa--");
-                          },
-                          child: Text("LinkedIn",
-                              style: GoogleFonts.exo(
-                                textStyle: TextStyle(
-                                    decoration: TextDecoration.underline),
-                                fontSize: 20,
-                                color: Colors.black54,
-                              )),
-                        ),
-                      ],
-                    )
-                  ]),
-            ),
-          ],
-        ),
+                      ),
+                      Text("     |     ",
+                          style: GoogleFonts.exo(
+                            fontSize: 20,
+                            color: Colors.black54,
+                          )),
+                      InkWell(
+                        onTap: () {
+                          launchUrlString(
+                              "https://linkedin.com/in/ahmedmustafa--");
+                        },
+                        child: Text("LinkedIn",
+                            style: GoogleFonts.exo(
+                              textStyle: TextStyle(
+                                  decoration: TextDecoration.underline),
+                              fontSize: 20,
+                              color: Colors.black54,
+                            )),
+                      ),
+                    ],
+                  )
+                ]),
+          ),
+        ],
       ),
     );
   }
