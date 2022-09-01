401b1edec9aba36942e684a33a131aaddbdbd349
Ahmed Mustafa <ahmedmustafa5832@gmail.com>
2022-09-01 16:44:15 -0400

    projects add

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/rendering.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'ProjectCard.dart';
 import 'colors.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,61 +14,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Widget> views = const [
-    Center(
-      child: Text('Home'),
-    ),
-    Center(
-      child: Text('Account'),
-    ),
-    Center(
-      child: Text('Settings'),
-    ),
-  ];
-
-  int selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: Row(
-      //   children: [
-      //     SideNavigationBar(
-      //       selectedIndex: selectedIndex,
-      //       items: const [
-      //         SideNavigationBarItem(
-      //           icon: Icons.dashboard,
-      //           label: 'Home',
-      //         ),
-      //         SideNavigationBarItem(
-      //           icon: Icons.person,
-      //           label: 'Account',
-      //         ),
-      //         SideNavigationBarItem(
-      //           icon: Icons.settings,
-      //           label: 'Settings',
-      //         ),
-      //       ],
-      //       theme: SideNavigationBarTheme(
-      //         backgroundColor: Colors.white,
-      //         togglerTheme: SideNavigationBarTogglerTheme.standard(),
-      //         itemTheme: SideNavigationBarItemTheme.standard(),
-      //         dividerTheme: SideNavigationBarDividerTheme.standard(),
-      //       ),
-      //       onTap: (index) {
-      //         setState(() {
-      //           selectedIndex = index;
-      //         });
-      //       },
-      //     ),
-      //     Expanded(
-      //         child: Column(
-      //       children: [Main()],
-      //     )
-      //         // child: views.elementAt(selectedIndex),
-      //         )
-      //   ],
-      // ),
       appBar: AppBar(
           toolbarHeight: 100,
           title: Padding(
@@ -141,8 +90,63 @@ class _HomeState extends State<Home> {
   }
 }
 
-class Main extends StatelessWidget {
+class Main extends StatefulWidget {
   const Main({Key? key}) : super(key: key);
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  String text = "A Problem Solver";
+  bool? animate;
+  bool? textAppear;
+
+  void showInfoProject(bool animate) {
+    setState(() {
+      if (animate) {
+        Future.delayed(const Duration(milliseconds: 300), () {
+          setState(() {
+            textAppear = true;
+          });
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    animate = false;
+    textAppear = false;
+    showInfoProject(animate!);
+
+    var array = [
+      "A Software Engineer",
+      "A Problem Solver",
+      "An ML Enthusiast",
+      "A Data Analyst"
+    ];
+
+    // const oneSec = Duration(seconds: 1);
+    // // Timer.periodic(oneSec, (Timer t) {
+    // for (int j = 0; j < array.length; j++) {
+    //   String str = array[j];
+    //   var split = str.split("");
+    //   String newText = "";
+    //   for (int i = 0; i < split.length; i++) {
+    //     Timer(Duration(seconds: 3), () {
+    //       print("ji");
+
+    //       setState(() {
+    //         newText += split[i];
+    //         text = newText;
+    //       });
+    //     });
+    //   }
+    // }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -152,10 +156,196 @@ class Main extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          Home(mediaHeight, mediaWidth, context),
+          Home(mediaHeight, mediaWidth, text, context),
           About(mediaHeight, mediaWidth, context),
           Skills(mediaHeight, mediaWidth, context),
+          Projects(mediaHeight, mediaWidth, context),
         ],
+      ),
+    );
+  }
+
+  Container Projects(
+      double mediaHeight, double mediaWidth, BuildContext context) {
+    return Container(
+      height: mediaHeight,
+      width: mediaWidth,
+      child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(100, 0, 0, 0),
+          child: Text("Projects",
+              style: GoogleFonts.exo(
+                  textStyle: Theme.of(context).textTheme.headline4,
+                  fontSize: 35,
+                  fontWeight: FontWeight.w700,
+                  color: primaryColor,
+                  decoration: TextDecoration.overline)),
+        ),
+        Expanded(
+          child: ListView(
+            children: [
+              ProjectCard2(
+                  mediaHeight,
+                  mediaWidth,
+                  "Silent Screamer",
+                  "This game was developed as a term project for the course Advanced Software Engineering - Winter 2022. It is developed using the Unity framework and C#",
+                  "https://github.com/muzztafa/ASEProject",
+                  "images/SilentScreamer.png"),
+              ProjectCard2(
+                  mediaHeight,
+                  mediaWidth,
+                  "Breathing App",
+                  "A customizable breathing app made with love and Java for Android. It comes with configurable pre-set modes and can be downloaded from the App Store",
+                  "https://github.com/muzztafa/Breathing-App-",
+                  "images/breathingapp_horizontal.png"),
+              ProjectCard2(
+                  mediaHeight,
+                  mediaWidth,
+                  "Web Search Engine",
+                  "It's a console based web search engine created on Java. Uses Boyer Moore Algorithm for page ranking and Edit Distance for word suggestions.",
+                  "https://github.com/muzztafa/Web-Search-Engine",
+                  "images/wse.gif"),
+              ProjectCard2(
+                  mediaHeight,
+                  mediaWidth,
+                  "FTP Protocol",
+                  "A small implementation of the FTP protocol in C. Uses sockets for connects and data tranfers. NO PIPES are used in this implementation.",
+                  "https://github.com/muzztafa/FTP-Protocol/tree/master",
+                  "images/ftp.gif"),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Padding ProjectCard(
+      double mediaHeight, double mediaWidth, String title, String url) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+          mediaHeight * 0.1, mediaHeight * 0.1, mediaHeight * 0.1, 0),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 5,
+              spreadRadius: 3,
+            )
+          ],
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        child: SizedBox(
+          height: mediaWidth * 0.15,
+          width: mediaWidth * 0.25,
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("images/SilentScreamer.png"),
+              ),
+              Align(alignment: Alignment.topCenter, child: Text("HELLO")),
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 6),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    child: Icon(
+                      Icons.link,
+                      color: primaryColor,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding ProjectCard2(double mediaHeight, double mediaWidth, String title,
+      String desc, String url, String image) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+          mediaHeight * 0.01, mediaHeight * 0.1, mediaHeight * 0.01, 0),
+      child: Card(
+        child: Container(
+          height: mediaHeight * 0.35,
+          color: Colors.white,
+          child: Row(
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Expanded(
+                    child: Image.asset(image),
+                    flex: 2,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: ListTile(
+                          title: Text(title,
+                              style: GoogleFonts.exo(
+                                  textStyle:
+                                      Theme.of(context).textTheme.headline4,
+                                  fontSize: mediaWidth * 0.03,
+                                  fontWeight: FontWeight.w700,
+                                  color: primaryColor,
+                                  decoration: TextDecoration.lineThrough)),
+                          subtitle: Text(desc,
+                              style: GoogleFonts.exo(
+                                textStyle:
+                                    Theme.of(context).textTheme.bodyMedium,
+                                fontSize: mediaWidth * 0.015,
+                                fontWeight: FontWeight.w500,
+                                color: primaryColor,
+                              )),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 5,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              child: Text("Github",
+                                  style: GoogleFonts.exo(
+                                    textStyle:
+                                        Theme.of(context).textTheme.bodyMedium,
+                                    fontSize: mediaHeight * 0.03,
+                                    fontWeight: FontWeight.w500,
+                                    color: primaryColor,
+                                  )),
+                              onPressed: () {
+                                launchUrlString(url);
+                              },
+                            ),
+                            SizedBox(
+                              width: 8,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                flex: 8,
+              ),
+            ],
+          ),
+        ),
+        elevation: 8,
+        margin: EdgeInsets.all(10),
       ),
     );
   }
@@ -335,7 +525,8 @@ class Main extends StatelessWidget {
     );
   }
 
-  Container Home(double mediaHeight, double mediaWidth, BuildContext context) {
+  Container Home(double mediaHeight, double mediaWidth, String text,
+      BuildContext context) {
     return Container(
       height: mediaHeight,
       width: mediaWidth,
@@ -378,7 +569,7 @@ class Main extends StatelessWidget {
                         fontWeight: FontWeight.w700,
                         color: primaryColor,
                       )),
-                  Text("A Problem Solver.",
+                  Text(text,
                       style: GoogleFonts.exo(
                         textStyle: Theme.of(context).textTheme.headline4,
                         fontSize: 35,
@@ -432,40 +623,5 @@ class Main extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class HoverButton extends StatefulWidget {
-  const HoverButton({Key? key}) : super(key: key);
-
-  @override
-  State<HoverButton> createState() => _HoverButtonState();
-}
-
-class _HoverButtonState extends State<HoverButton> {
-  @override
-  Widget build(BuildContext context) {
-    bool isHover = false;
-    return AnimatedContainer(
-        height: 70,
-        duration: Duration(milliseconds: 200),
-        padding: EdgeInsets.only(
-            top: (isHover) ? 25 : 30.0, bottom: !(isHover) ? 25 : 30),
-        child: Container(
-          height: 30,
-          color: Colors.blue,
-          child: InkWell(
-            onTap: () {},
-            child: Text("Hover Button"),
-            onHover: (val) {
-              print("Val--->{}$val");
-              setState(() {
-                isHover = val;
-              });
-            },
-          ),
-          /*val--->true when user brings in mouse
-           val---> false when brings out his mouse*/
-        ));
   }
 }
